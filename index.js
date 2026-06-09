4f29e837f32c0c8e6369a4bced049c572e866a1d
Harsh Srivastava <harshcsrivastava@gmail.com>
2026-06-09 21:44:56 +0530

    fix: update deployment host to use secrets and correct status message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.get("/", (req, res) => {
     return res.status(200).json({
         message: "Hello, World! Github Actions with traditional way",
-        status: "Github-Actions-Deployment YEeeeeah"
+        status: "Github-Actions-Deployment Successful"
     })
 })
 
