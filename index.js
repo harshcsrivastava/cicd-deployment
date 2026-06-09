171382bf9b16cb5a98d27c9ece1b117c239a25e8
Harsh Srivastava <harshcsrivastava@gmail.com>
2026-06-09 20:51:24 +0530

    Github Actions Bug Fixes with new VM

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const app = express()
 app.get("/", (req, res) => {
     return res.status(200).json({
         message: "Hello, World! Github Actions with traditional way",
+        status: "Github-Actions-Deployment"
     })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
 })
