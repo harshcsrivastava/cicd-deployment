d3c10c900a10d7547125497be34c6464b5a8789e
Harsh Srivastava <harshcsrivastava@gmail.com>
2026-06-09 20:54:24 +0530

    fix: update greeting message and log output format

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express()
 app.get("/", (req, res) => {
     return res.status(200).json({
         message: "Hello, World! Github Actions with traditional way",
-        status: "Github-Actions-Deployment"
+        status: "Github-Actions-Deployment YEeeeeah"
     })
 })
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
